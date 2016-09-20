aa718814eb96532c3195881cd4dd9646778e51d4
CFLATED <k1hiiargi@gmail.com>
2016-09-21 05:34:07 +0900

    アイコンの表示位置を変更

--- app/src/main/java/tempakunoshiro/automaticotakumatching/HirobaActivity.java
+++ app/src/main/java/tempakunoshiro/automaticotakumatching/HirobaActivity.java
@@ -26,12 +26,25 @@ public class HirobaActivity extends AppCompatActivity {
     Point imgSize;
     Point actSize;
 
+    final int rows = 8;
+    final int cols = 12;
+    Long[][] idTable = new Long[rows][cols];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                idTable[i][j] = new Long(0);
+            }
+        }
         userList = new ArrayList();
         userPos = new HashMap<Long, Point>();
-        super.onCreate(savedInstanceState);
+        imgSize = new Point();
+
         setContentView(R.layout.activity_hiroba);
+
+
 
         //debug code
         for(int i = 0; i <= 10; i++)
@@ -42,6 +55,8 @@ public class HirobaActivity extends AppCompatActivity {
             long id = bundle.getLong("ID");
             System.out.println(id);
         }
+
+        Update();
     }
 
     //debug本来はプリファレンス
@@ -51,6 +66,8 @@ public class HirobaActivity extends AppCompatActivity {
         lay = (RelativeLayout)findViewById(R.id.hiroba);
         //View初期化
         System.out.println("laywidth:" + lay.getWidth());
+        System.out.println("laheight:" + lay.getHeight());
+        imgSize.set(lay.getWidth() / rows, lay.getHeight() / cols);
         lay.removeAllViews();
 
         //画面サイズ取得
@@ -71,16 +88,10 @@ public class HirobaActivity extends AppCompatActivity {
         img = new ImageView(this);
         name = new TextView(this);
 
-        RelativeLayout.LayoutParams ilp =
-                new  RelativeLayout.LayoutParams(
-                        android.app.ActionBar.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT
-                );
-
         RelativeLayout.LayoutParams tlp =
                 new  RelativeLayout.LayoutParams(
                         android.app.ActionBar.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT
+                       ViewGroup.LayoutParams.WRAP_CONTENT
                 );
 
         //アイコン画像セット
@@ -88,48 +99,47 @@ public class HirobaActivity extends AppCompatActivity {
         name.setText(u.getName());
 
         //位置設定
+        int x = 0, y =0;
         if(u.getId() == myId){
-            ilp.addRule(RelativeLayout.CENTER_HORIZONTAL);
-            ilp.addRule(RelativeLayout.CENTER_VERTICAL);
+            x = 3;
+            y = 5;
         }else{
             Random rnd = new Random();
-            if(!userPos.containsKey(u.getId())) {
-                int x, y;
-                x = rnd.nextInt(actSize.x - imgSize.x * 2);
-                y = rnd.nextInt(actSize.y - imgSize.y * 2);
-                userPos.put(u.getId(), new Point(x, y));
-            }
-            img.setTranslationX(userPos.get(u.getId()).x);
-            img.setTranslationY(userPos.get(u.getId()).y);
-            name.setTranslationX(userPos.get(u.getId()).x);
-            name.setTranslationY(userPos.get(u.getId()).y);
+            x = rnd.nextInt(rows - 1);
+            y = rnd.nextInt(cols - 1);
+
         }
-        tlp.addRule(RelativeLayout.BELOW, img.getId());
-        tlp.addRule(RelativeLayout.ALIGN_START, img.getId());
+
+        x *= imgSize.x;
+        y *= imgSize.y;
+        userPos.put(u.getId(), new Point(x, y));
+        img.setTranslationX(userPos.get(u.getId()).x);
+        img.setTranslationY(userPos.get(u.getId()).y);
+        name.setTranslationX(userPos.get(u.getId()).x);
+        name.setTranslationY(userPos.get(u.getId()).y + imgSize.y / 2);
 
         //クリック時Activity移動動作
         final long id = u.getId();
-        final long iid = img.getId();
         View.OnClickListener cl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addList();
-                //Update();
+                Update();
                 //debug 本来はProfileへ移動
                 Intent intent = new Intent(HirobaActivity.this, ProfileActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putLong("ID", id);
-                System.out.println("imgid:" + iid);
+                System.out.println("userid:" + id);
                 intent.putExtras(bundle);
-                startActivity(intent);
+                //startActivity(intent);
             }
         };
 
         img.setOnClickListener(cl);
         name.setOnClickListener(cl);
 
+        lay.addView(img);
         lay.addView(name, tlp);
-        lay.addView(img, ilp);
+
 
 
 
@@ -147,9 +157,9 @@ public class HirobaActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         try {
             lay = (RelativeLayout)findViewById(R.id.hiroba);
-            ImageView tmp = (ImageView) findViewById(R.id.myImage);
-            imgSize = new Point();
-            imgSize.set(tmp.getWidth(), tmp.getHeight());
+            //ImageView tmp = (ImageView) findViewById(R.id.myImage);
+            //imgSize = new Point();
+            //imgSize.set(tmp.getWidth(), tmp.getHeight());
             actSize = new Point();
             actSize.set(lay.getWidth(), lay.getHeight());
             //System.out.println("sizex" + lay.getWidth() + "sizey" + lay.getHeight());
@@ -163,7 +173,7 @@ public class HirobaActivity extends AppCompatActivity {
     int n = 1;
     String namae = "name";
     private void addList(){
-        namae += "e";
+        //namae += "e";
         userList.add(new User(n, namae, "taro", "jiro", "k1", "fuck"));
         n++;
    }
