07dad51fe3e7bbab4dc250e5373ed7d8c0bd2663
foldori <origaminomiko@gmail.com>
2016-09-23 16:34:53 +0900

    とりあえずサンプルを移植

---
+++ app/src/main/java/tempakunoshiro/automaticotakumatching/WifiDirectBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package tempakunoshiro.automaticotakumatching;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+    public WifiDirectBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
